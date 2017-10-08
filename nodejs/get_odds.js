3e19c9aad1fc106b222ced0a695cda297f5ebb72
Harry Sadler <harrysadlermusic@gmail.com>
2017-10-07 19:29:30 -0700

    add get_odds solution for nodejs

---
+++ nodejs/get_odds.js
@@ -0,0 +1,11 @@
+
+// get odds from list of integers
+var nums = process.argv.slice(2).map(function(argItem) {
+    return parseInt(argItem);
+});
+
+var odds = nums.filter(function(num) {
+    return num % 2 === 1 || num % 2 === -1;
+});
+
+console.log(odds);
