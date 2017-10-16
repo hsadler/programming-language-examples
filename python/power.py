804e37aec2ed32606776d958e61e67e5278c8ed4
Harry Sadler <harrysadlermusic@gmail.com>
2017-10-15 23:13:41 -0700

    add python solution for 'power'

---
+++ python/power.py
@@ -0,0 +1,16 @@
+
+# given a base and exponent, calculate the power via recursion
+
+import sys
+
+base, exponent = float(sys.argv[1]), float(sys.argv[2])
+
+def power(base, exp):
+    if exp == 0:
+        return 1
+    else:
+        return base * power(base, exp - 1)
+
+result = power(base=base, exp=exponent)
+
+print int(result) if result % 1 == 0 else result
