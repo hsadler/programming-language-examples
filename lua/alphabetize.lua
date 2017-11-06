e9fb13dde34b2dd36141386499d74cd4f53c55ab
Harry Sadler <harrysadlermusic@gmail.com>
2017-11-05 17:57:16 -0800

    add lua solution for alphabetize

--- lua/alphabetize.lua
+++ lua/alphabetize.lua
@@ -1,3 +1,18 @@
 
 -- alphabetize a list of strings
 
+string_list = {}
+i = 1
+
+while arg[i] ~= nil do
+    table.insert(string_list, arg[i])
+    i = i + 1
+end
+
+function compare_strings(a, b)
+    return a < b
+end
+
+table.sort(string_list, compare_strings)
+
+io.write(table.concat(string_list, ' ') .. '\n')
