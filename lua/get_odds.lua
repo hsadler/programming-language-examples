7a6a1b5cccccfdb4a4ca6a9093dec91376041a08
Harry Sadler <harrysadlermusic@gmail.com>
2017-11-07 17:48:06 -0800

    fix indentation for some lua files

--- lua/get_odds.lua
+++ lua/get_odds.lua
@@ -4,9 +4,9 @@
 odds = {}
 
 for k, v in pairs(arg) do
-	if k > 0 and v % 2 ~= 0 then
-		table.insert(odds, v)
-	end
+    if k > 0 and v % 2 ~= 0 then
+        table.insert(odds, v)
+    end
 end
 
 io.write(table.concat(odds, ' ') .. '\n')
